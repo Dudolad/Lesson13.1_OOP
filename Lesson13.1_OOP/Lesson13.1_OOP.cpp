15b3dd1cdaefed18fa5fefc3ecce494f11768382
dudolad_yaroslav <dudolad2009@gmail.com>
2025-10-29 19:10:45 +0200

    4

--- Lesson13.1_OOP/Lesson13.1_OOP.cpp
+++ Lesson13.1_OOP/Lesson13.1_OOP.cpp
@@ -1,20 +1,10 @@
-// Lesson13.1_OOP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Shop.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Shop ItStep;
+    ItStep.addProduct(new Products::Fun::Book("C++", 1000, "KSD", 10, "Straustrub", "Study", 200));
+    ItStep.addProduct(new Products::Sport::SportsEquipment("Ball", 5000, "China", 2, "Football", 1));
+    ItStep.showList();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
