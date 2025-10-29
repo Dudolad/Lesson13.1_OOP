058abb4ba7b40e942ee3acdd075b9f7f45eda3f3
dudolad_yaroslav <dudolad2009@gmail.com>
2025-10-29 18:57:01 +0200

    3

--- Lesson13.1_OOP/SportsEquipment.cpp
+++ Lesson13.1_OOP/SportsEquipment.cpp
@@ -7,33 +7,45 @@ Products::Sport::SportsEquipment::SportsEquipment()
 }
 
 Products::Sport::SportsEquipment::SportsEquipment(std::string name, float price, std::string manufacture, int age, std::string kind, float weight)
-    : Product()
+    : Product(name,price, manufacture, age)
 {
+    this->kind = kind;
+    this->weight = weight;
 }
 
 void Products::Sport::SportsEquipment::setKind(std::string kind)
 {
+    this->kind = kind;
 }
 
 void Products::Sport::SportsEquipment::setWeight(float weight)
 {
+    this->weight = weight;
 }
 
 std::string Products::Sport::SportsEquipment::getKind() const
 {
-    return std::string();
+    return kind;
 }
 
 float Products::Sport::SportsEquipment::getWeight() const
 {
-    return 0.0f;
+    return weight;
 }
 
 void Products::Sport::SportsEquipment::show() const
 {
+    using namespace std;
+    cout << "\tSPORT EQUIPMENT\n";
+    cout << "Name: " << name << endl;
+    cout << "Manufacture: " << manufacture << endl;
+    cout << "Kind: " << kind << endl;
+    cout << "Price: " << price << endl;
+    cout << "Age: " << age << endl;
+    cout << "Weight: " << weight << endl;
 }
 
 float Products::Sport::SportsEquipment::calcPrice() const
 {
-    return 0.0f;
+    return price + (age < 7 ? price * 0.1 : 0);
 }
